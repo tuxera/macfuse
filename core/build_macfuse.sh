3c7929fbd491d60f91ecb7b72fea5d3011b845dc
Amit Singh <asingh@gmail.com>
2007-09-15 20:03:36 +0000

    Added build harness script.
    git-svn-id: http://macfuse.googlecode.com/svn/trunk@482 febdc8c4-1326-0410-abd6-27cb15717650

---
+++ core/build_macfuse.sh
@@ -0,0 +1,55 @@
+#! /bin/sh
+
+UNAME=/usr/bin/uname
+
+os_name=`$UNAME -s`
+os_codename="Unknown"
+os_release=`$UNAME -r`
+src_dir="/dev/null"
+
+if [ "$os_name" != "Darwin" ]
+then
+    echo "This script can only be run on Darwin"
+    exit 1
+fi
+
+case "$os_release" in
+  8*)
+      src_dir="./10.4/fusefs/"
+      os_codename="Tiger"
+  ;;
+  9*)
+      src_dir="./10.5/fusefs/"
+      os_codename="Leopard"
+  ;;
+  *)
+      echo "Unsupported Mac OS X release $os_release"
+      exit 1
+  ;;
+esac
+
+sudo true # Need root password to set permissions for output files.
+if [ $? -ne 0 ]
+then
+    echo "Administrator password is required to build MacFUSE."
+    exit 1
+fi
+
+echo "Initiating Universal build of MacFUSE for Mac OS X \"$os_codename\""
+
+pushd . > /dev/null
+
+cd "$src_dir"
+if [ $? -ne 0 ]
+then
+    echo "*** failed to access MacFUSE source"
+    exit 1
+fi
+
+sudo xcodebuild -target All -configuration Release
+
+ret=$?
+
+popd > /dev/null
+
+exit $ret
