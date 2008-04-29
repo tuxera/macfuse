09aecedcebd5c2d8c0a7825a19984dded4874527
Amit Singh <asingh@gmail.com>
2008-04-29 06:35:31 +0000

    Added build cleanup feature to build_macfuse.sh
    git-svn-id: http://macfuse.googlecode.com/svn/trunk@878 febdc8c4-1326-0410-abd6-27cb15717650

--- core/build_macfuse.sh
+++ core/build_macfuse.sh
@@ -2,10 +2,13 @@
 # Copyright (C) 2006-2007 Amit Singh. All Rights Reserved.
 #
 
+DIRNAME=/usr/bin/dirname
+RM=/bin/rm
 UNAME=/usr/bin/uname
 
 os_name=`$UNAME -s`
 os_codename="Unknown"
+this_dir=`$DIRNAME $0`
 
 os_release=`$UNAME -r`
 if [ "$1" != "" ]
@@ -22,12 +25,28 @@ then
 fi
 
 case "$os_release" in
+  0)
+      pushd . > /dev/null
+      cd "$this_dir"
+      if [ $? -ne 0 ]
+      then
+          echo "$this_dir" is not a valid MacFUSE source directory
+          exit 1
+      fi
+      echo "Cleaning up any previous MacFUSE builds"
+      sudo $RM -rf "$this_dir"/10.4/fusefs/build/
+      sudo $RM -rf "$this_dir"/10.5/fusefs/build/
+      sudo $RM -rf "$this_dir"/sdk-objc/build/
+      sudo $RM -rf "/tmp/macfuse-core-10.*-*"
+      popd > /dev/null
+      exit 0
+  ;;
   8*)
-      src_dir="./10.4/fusefs/"
+      src_dir="$this_dir/10.4/fusefs/"
       os_codename="Tiger"
   ;;
   9*)
-      src_dir="./10.5/fusefs/"
+      src_dir="$this_dir/10.5/fusefs/"
       os_codename="Leopard"
   ;;
   *)
