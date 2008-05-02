daff0e555118124e9fd6cd20c3c379e52bb50631
Amit Singh <asingh@gmail.com>
2008-05-02 08:35:21 +0000

    Improved build script.
    git-svn-id: http://macfuse.googlecode.com/svn/trunk@926 febdc8c4-1326-0410-abd6-27cb15717650

--- core/build_macfuse.sh
+++ core/build_macfuse.sh
@@ -2,13 +2,25 @@
 # Copyright (C) 2006-2007 Amit Singh. All Rights Reserved.
 #
 
+CUT=/usr/bin/cut
 DIRNAME=/usr/bin/dirname
 RM=/bin/rm
 UNAME=/usr/bin/uname
 
 os_name=`$UNAME -s`
 os_codename="Unknown"
-this_dir=`$DIRNAME $0`
+
+is_absolute_path=`echo "$0" | $CUT -c1`
+if [ "$is_absolute_path" = "/" ]
+then
+    macfuse_dir="`$DIRNAME $0`/.."
+else
+    macfuse_dir="`pwd`/`$DIRNAME $0`/.."
+fi
+pushd . > /dev/null
+cd "$macfuse_dir" || exit 1
+macfuse_dir=`pwd`
+popd > /dev/null
 
 os_release=`$UNAME -r`
 if [ "$1" != "" ]
@@ -26,12 +38,6 @@ fi
 
 case "$os_release" in
   0)
-      pushd . > /dev/null
-      if [ $? -ne 0 ]
-      then
-          echo "$this_dir" is not a valid MacFUSE source directory
-          exit 1
-      fi
       sudo true # Need root password to delete previous builds
       if [ $? -ne 0 ]
       then
@@ -39,19 +45,18 @@ case "$os_release" in
           exit 1
       fi
       echo "Cleaning up any previous MacFUSE builds"
-      sudo $RM -rf "$this_dir"/10.4/fusefs/build/
-      sudo $RM -rf "$this_dir"/10.5/fusefs/build/
-      sudo $RM -rf "$this_dir"/sdk-objc/build/
+      sudo $RM -rf "$macfuse_dir"/core/10.4/fusefs/build/
+      sudo $RM -rf "$macfuse_dir"/core/10.5/fusefs/build/
+      sudo $RM -rf "$macfuse_dir"/core/sdk-objc/build/
       sudo $RM -rf "/tmp/macfuse-core-10.*-*"
-      popd > /dev/null
       exit 0
   ;;
   8*)
-      src_dir="$this_dir/10.4/fusefs/"
+      src_dir="$macfuse_dir/core/10.4/fusefs/"
       os_codename="Tiger"
   ;;
   9*)
-      src_dir="$this_dir/10.5/fusefs/"
+      src_dir="$macfuse_dir/core/10.5/fusefs/"
       os_codename="Leopard"
   ;;
   *)
