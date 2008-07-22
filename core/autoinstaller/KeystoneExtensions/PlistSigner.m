4c7fb3335157a30191678a8888fb2f9acbaed0b0
Greg Miller <devjgm@gmail.com>
2008-07-22 00:56:44 +0000

    Changed signature calculation so that it should work on both Leopard and Tiger
    git-svn-id: http://macfuse.googlecode.com/svn/trunk@1109 febdc8c4-1326-0410-abd6-27cb15717650

--- core/autoinstaller/KeystoneExtensions/PlistSigner.m
+++ core/autoinstaller/KeystoneExtensions/PlistSigner.m
@@ -14,7 +14,14 @@ static NSString *const kSignatureKey = @"Signature";
 
 
 @interface PlistSigner (PrivateMethods)
+
 - (void)setPlist:(NSDictionary *)plist;
+
+// Returns an opaque blob of data generated from |plist|. This blob of data can
+// be used for signing and signature verification. However, it should NOT be
+// interpreted in any way.
+- (NSData *)blobFromDictionary:(NSDictionary *)plist;
+
 @end
 
 
@@ -51,12 +58,9 @@ static NSString *const kSignatureKey = @"Signature";
   NSData *signature = [mutablePlist objectForKey:kSignatureKey];
   [mutablePlist removeObjectForKey:kSignatureKey];
   
-  NSData *plistData = [NSPropertyListSerialization
-                       dataFromPropertyList:mutablePlist
-                       format:NSPropertyListXMLFormat_v1_0
-                       errorDescription:NULL];
+  NSData *blob = [self blobFromDictionary:mutablePlist];
   
-  return [signer_ isSignature:signature validForData:plistData];
+  return [signer_ isSignature:signature validForData:blob];
 }
 
 - (BOOL)signPlist {
@@ -65,12 +69,9 @@ static NSString *const kSignatureKey = @"Signature";
   NSMutableDictionary *mutablePlist = [[plist_ mutableCopy] autorelease];
   [mutablePlist removeObjectForKey:kSignatureKey];
   
-  NSData *plistData = [NSPropertyListSerialization
-                       dataFromPropertyList:mutablePlist
-                       format:NSPropertyListXMLFormat_v1_0
-                       errorDescription:NULL];
+  NSData *blob = [self blobFromDictionary:mutablePlist];
   
-  NSData *signature = [signer_ signData:plistData];
+  NSData *signature = [signer_ signData:blob];
   BOOL ok = NO;
   
   if (signature != nil) {
@@ -98,6 +99,32 @@ static NSString *const kSignatureKey = @"Signature";
 - (void)setPlist:(NSDictionary *)plist {
   [plist_ autorelease];
   plist_ = [plist copy];
+}
+
+- (NSData *)blobFromDictionary:(NSDictionary *)plist {
+  // In order to sign/very plists on both Tiger and Leoaprd, we need to
+  // serialize the plist into an NSData, but this will contain an XML comment
+  // of either "Apple" or "Apple Computer" (Tiger), which will screw up 
+  // signatures. So, we need to convert this XML to a big string, strip off
+  // the first two lines (which are comments), then return the remainder of the
+  // XML as a big data blob.
+  
+  NSData *plistData = [NSPropertyListSerialization
+                       dataFromPropertyList:plist
+                       format:NSPropertyListXMLFormat_v1_0
+                       errorDescription:NULL];
+  
+  NSString *plistString = [[[NSString alloc]
+                            initWithData:plistData
+                                encoding:NSUTF8StringEncoding] autorelease];
+  
+  NSArray *lines = [plistString componentsSeparatedByString:@"\n"];
+  
+  NSRange range = NSMakeRange(2, [lines count] - 2);
+  NSArray *trimmedLines = [lines subarrayWithRange:range];
+  
+  NSString *trimmedString = [trimmedLines componentsJoinedByString:@"\n"];
+  return [trimmedString dataUsingEncoding:NSUTF8StringEncoding];
 }
 
 @end
