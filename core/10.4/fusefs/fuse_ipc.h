953691aa2bdb5eb792ba58e36e1633de56744e72
Amit Singh <asingh@gmail.com>
2008-11-27 06:32:47 +0000

    Bits for FSESS_SPARSE.
    git-svn-id: http://macfuse.googlecode.com/svn/trunk@1495 febdc8c4-1326-0410-abd6-27cb15717650

--- core/10.4/fusefs/fuse_ipc.h
+++ core/10.4/fusefs/fuse_ipc.h
@@ -229,6 +229,8 @@ enum {
 #define FSESS_VOL_RENAME          0x02000000
 #define FSESS_XTIMES              0x04000000
 #define FSESS_AUTO_CACHE          0x08000000
+#define FSESS_NATIVE_XATTR        0x10000000
+#define FSESS_SPARSE              0x20000000
 
 static __inline__
 struct fuse_data *
